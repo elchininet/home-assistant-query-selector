6a163eaca64700f08301faeaa95f08202c86830b
Iván Pereira Muñoz <elchininet@gmail.com>
2026-02-20 20:40:18 +0100

    Prepare the library for Home Assistant 2026.3.x

--- tests/constants.ts
+++ tests/constants.ts
@@ -1,16 +1,22 @@
 export const BASE_URL = 'http://host.docker.internal:8123';
 
 export const SELECTORS = {
+    HOME_ASSISTANT: 'home-assistant',
     HA_SIDEBAR: 'ha-sidebar',
     HEADER_HISTORY: 'ha-top-app-bar-fixed',
     LINKS: 'ha-md-list > ha-md-list-item',
-    CLOSE_DIALOG: 'mwc-icon-button[title="Close"]',
+    CLOSE_DIALOG: 'ha-icon-button[slot="headerNavigationIcon"] ha-button[title="Close"]',
     ENTITY_CARD: 'hui-generic-entity-row',
     HA_MAP: 'ha-map',
     HU_VIEW_CONTAIER: 'hui-view-container',
-    DIALOG_HISTORY_BUTTON: 'mwc-icon-button[title="History"]',
+    DIALOG_HISTORY_BUTTON: 'ha-icon-button[slot="headerActionItems"] ha-button[title="History"]',
     DIALOG_HISTORY_PANEL: 'ha-more-info-history-and-logbook',
-    DIALOG_GO_BACK_BUTTON: 'ha-icon-button-prev[slot="navigationIcon"]',
-    DIALOG_CONFIG_BUTTON: 'mwc-icon-button[title="Settings"]',
-    DIALOG_SETTINGS_PANEL: 'ha-more-info-settings'
+    DIALOG_GO_BACK_BUTTON: 'ha-icon-button-prev[slot="headerNavigationIcon"] ha-icon-button',
+    DIALOG_CONFIG_BUTTON: 'ha-icon-button[slot="headerActionItems"] ha-button[title="Settings"]',
+    DIALOG_SETTINGS_PANEL: 'ha-more-info-settings',
+    HA_MORE_INFO_DIALOG: 'ha-more-info-dialog',
+    HA_DIALOG: 'ha-adaptive-dialog',
+    HA_DIALOG_CONTENT: '.content',
+    HA_MORE_INFO_INFO: 'ha-more-info-info',
+    HA_MORE_INFO_SETTINGS: 'ha-more-info-settings'
 };
